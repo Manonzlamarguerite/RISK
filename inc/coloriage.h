54ab4c32bfb0fe0558755f20966d49f3ba506cb2
SchizoBoy <cadonienzo@gmail.com>
2021-06-03 13:22:53 +0200

    merge

---
+++ inc/coloriage.h
@@ -0,0 +1,51 @@
+// Fichier header contenant prototypes des fonctions de coloriage des
+// territoires
+
+#ifndef COLORIAGE_H_
+#define COLORIAGE_H_
+
+void color_alaska(int color);
+void color_amerique_nord(int color);
+void color_alberta(int color);
+void color_ontario(int color);
+void color_quebec(int color);
+void color_amerique_est(int color);
+void color_amerique_ouest(int color);
+void color_amerique_centrale(int color);
+void color_groenland(int color);
+void color_venezuela(int color);
+void color_perou(int color);
+void color_bresil(int color);
+void color_argentine(int color);
+void color_japon(int color);
+void color_kamtchatka(int color);
+void color_tchita(int color);
+void color_mongolie(int color);
+void color_chine(int color);
+void color_syberie(int color);
+void color_oural(int color);
+void color_yakoutie(int color);
+void color_siam(int color);
+void color_australie_est(int color);
+void color_australie_ouest(int color);
+void color_nouvelle_guinee(int color);
+void color_indonesie(int color);
+void color_madagascar(int color);
+void color_inde(int color);
+void color_islande(int color);
+void color_grande_bretagne(int color);
+void color_scandinavie(int color);
+void color_europe_occidentale(int color);
+void color_europe_du_nord(int color);
+void color_ukraine(int color);
+void color_europe_du_sud(int color);
+void color_moyen_orient(int color);
+void color_afghanistan(int color);
+void color_afrique_du_sud(int color);
+void color_afrique_de_l_est(int color);
+void color_afrique_du_nord(int color);
+void color_congo(int color);
+void color_egypte(int color);
+void color_pays(int pays, int flag);
+
+#endif
