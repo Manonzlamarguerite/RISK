b0d9c683399b81ea4a50800263e335372e1522e0
SchizoBoy <cadonienzo@gmail.com>
2021-06-04 11:53:05 +0200

    update jeu

--- src/jeu.c
+++ src/jeu.c
@@ -10,33 +10,55 @@
 #include "attaque.h"
 #include "graphisme.h"
 
-// Tour tiré au hasard
-int tour;
-
-// Troupes de chacuns
-int troupes1 = 40;
-int troupes2 = 40;
-
 void jeu(){
 
-  MLV_Sound *boom;
-  liste_territoires global_list = initialisation_jeu();
-  int i;
+  // TLA representant les territoires adjacents
+  liste_voisin global_carte = creation_tla();
+  // Liste représentant les territoires
+  liste_territoires global_terr = initialisation_jeu();
+  int tour = rand()%2;  // Tour tiré au hasard
+  int troupes1 = 40;    // Troupes du J1
+  int troupes2 = 40;    // Troupes du J2
+  int terr1 = 21;       // Territoires du J1
+  int terr2 = 21        // Territoires du J2
+  int win = 0;          // Variable de fin de jeu
+  int i, cont1, cont1;
 
-  if(MLV_init_audio()){
-    fprintf(stderr, "L'infrasctructure audio s'est chié");
-    exit(1);
+  // Tant qu'aucun n'a gagné
+  while(!win){
+    // Init graphisme
+    init_graphisme();
+
+    // On regarde le nombre de continent
+    nb_cont_J1 = nb_continent(1, global_terr);
+    nb_cont_J1 = nb_continent(2, global_terr);
+
+    // On colorie les territoires
+    for(i = 0; i < 42; i++){
+      color_pays(i, global_terr[i].appartenance);
+    }
+
+    // On affiche les troupes
+    affichage_troupe(global_carte, global_terr);
+    // On affiche les élements du bandeau
+    affichage_bandeau_droite(terr1, terr2,
+      cont1, cont2,
+      troupes1, troupes2,
+      tour
+    );
+
+    //##################################
+    //########  Première phase  ########
+    //##################################
+
+    //##################################
+    //########  Deuxième phase  ########
+    //##################################
+
+    //###################################
+    //########  Troisième phase  ########
+    //###################################
+
+    MLV_wait_mouse(NULL, NULL);
   }
-
-  boom = MLV_load_sound("./ressources/OST/bruits/sf_explosion_01.wav");
-
-  tour = rand()%2;
-
-  init_graphisme();
-  affichage_bandeau_droite(0, 0, 0, 0, 0, 0, tour);
-
-  for(i = 0; i < 42; i++){
-    color_pays(i, global_list[i].appartenance);
-  }
-
 }
